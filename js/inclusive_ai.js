988dd8f31a20953e3459dcd6d16bac1d6b6ce130
Atenea <avadillog0504@gmail.com>
2024-11-20 11:13:49 +0100

    JS image fetch

--- js/inclusive_ai.js
+++ js/inclusive_ai.js
@@ -3,6 +3,14 @@ jQuery(document).ready(function($) {
         var button = $(this);
         var description = $("textarea[name='acf[field_6731f5a2ecd8f]']").val();
 
+        const postImageDiv = $('#postimagediv');
+        const images = postImageDiv.find('img');
+        const imageUrls = [];
+
+        images.each(function() {
+            imageUrls.push($(this).attr('src'));
+        });
+
         $('#saving-description').css('display', 'inline-block');
         button.hide();
 
